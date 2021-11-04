44ec91a869820ac96fdf4f60403c52c3417d357a
Idriss Neumann <idriss.neumann@comwork.io>
2021-11-04 23:01:52 +0100

    Fix: remove deploy

--- ci/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-REPO_PATH="/home/centos/github-dynamic-changelog/"
-
-cd "${REPO_PATH}" && git pull origin main || :
-echo "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}" > .env
-docker-compose -f docker-compose-intra.yml up -d --force-recreate
