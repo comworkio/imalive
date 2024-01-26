c562e36398b8fc90cca0090c29a8d1eecd7e19ba
Ayoub Abidi <mrayoubabidi@gmail.com>
2024-01-26 10:04:02 +0100

    fix: rename HEART_BIT_LOG_JSON => LOG_FORMAT

--- src/utils/heartbit.py
+++ src/utils/heartbit.py
@@ -7,12 +7,12 @@ from utils.common import is_true
 
 WAIT_TIME = int(os.environ['WAIT_TIME'])
 NODE_NAME = os.environ['IMALIVE_NODE_NAME']
-HEART_BIT_LOG_JSON = os.environ['HEART_BIT_LOG_JSON']
+LOG_FORMAT = os.environ['LOG_FORMAT']
 
 def heartbit():
     while True:
         vdate = datetime.now()
-        if is_true(HEART_BIT_LOG_JSON):
+        if is_true(LOG_FORMAT):
             print(all_metrics())
         else:
             print("[{}][{}] I'm alive! metrics = {}".format(vdate.isoformat(), NODE_NAME, all_metrics()))
