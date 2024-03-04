9e5fd0f8b57b5ff040f84cf442f5fa434c78cfd5
Idriss Neumann <idriss.neumann@comwork.io>
2024-03-04 18:11:34 +0100

    Prepare utils for metrics counters

---
+++ src/utils/counter.py
@@ -0,0 +1,12 @@
+from utils.otel import get_otel_meter
+
+def create_counter(name, description):
+    return get_otel_meter().create_counter(
+        name = name,
+        description = description,
+        unit = "1",
+        value_type = int
+    )
+
+def increment_counter(counter, tid):
+    counter.add(1, {"tid": tid})
