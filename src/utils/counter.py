527d9ff060c125cc8d6912b6a8446ac30b61356b
Idriss Neumann <idriss.neumann@comwork.io>
2024-03-10 20:55:30 +0100

    Add prometheus client counter

--- src/utils/counter.py
+++ src/utils/counter.py
@@ -1,11 +1,17 @@
+from prometheus_client import Counter
+
 from utils.otel import get_otel_meter
 
 def create_counter(name, description):
-    return get_otel_meter().create_counter(
-        name = name,
-        description = description,
-        unit = "1"
-    )
+    return {
+        'otel': get_otel_meter().create_counter(
+                    name = name,
+                    description = description,
+                    unit = "1"
+                ),
+        'prom': Counter(name, description)
+    }
 
 def increment_counter(counter, tid):
-    counter.add(1, {"tid": tid})
+    counter['otel'].add(1, {"tid": tid})
+    counter['prom'].inc()
