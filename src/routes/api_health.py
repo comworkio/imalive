e5693b306148beb9b1c7da50d019a3a50a6f73f9
Idriss Neumann <idriss.neumann@comwork.io>
2024-03-10 20:45:12 +0100

    Add other counters

--- src/routes/api_health.py
+++ src/routes/api_health.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter
 
+from utils.cid import get_current_cid
+from utils.counter import create_counter, increment_counter
 from utils.otel import get_otel_tracer
 from utils.health import health
 
 router = APIRouter()
+_counter = create_counter("health_api_counter", "Health API counter")
 
 @router.get("")
 def get_health():
     with get_otel_tracer().start_as_current_span("imalive-health-get-route"):
+        increment_counter(_counter, get_current_cid())
         return health()
 
 @router.post("")
 def post_health():
     with get_otel_tracer().start_as_current_span("imalive-health-post-route"):
+        increment_counter(_counter, get_current_cid())
         return health()
--- src/routes/api_metrics.py
+++ src/routes/api_metrics.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
 
+from utils.cid import get_current_cid
+from utils.counter import create_counter, increment_counter
 from utils.otel import get_otel_tracer
 from utils.metrics import all_metrics
 
 router = APIRouter()
+_counter = create_counter("metrics_api_counter", "Health API counter")
 
 @router.get("")
 def get_metrics():
     with get_otel_tracer().start_as_current_span("imalive-metrics-route"):
+        increment_counter(_counter, get_current_cid())
         return all_metrics()
